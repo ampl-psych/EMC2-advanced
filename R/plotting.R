ae69edacbdb69fad5ac9d9dea1fc5c41fc527cd9
andrewheathcote <andrew.heathcote@newcastle.edu.au>
2024-07-08 13:55:06 +1000

    Fixed bug in the way factors is handled in plot_fit and corresponding error in help

--- R/plotting.R
+++ R/plotting.R
@@ -810,7 +810,6 @@ plot_fit_choice <- function(data,pp,subject=NULL,factors=NULL,functions=NULL,
 #' @param factors Character vector of factors in data to display separately. If
 #' NULL (default) use names of all columns in data except "trials","R", and "rt".
 #' Omitted factors are aggregated over. If NA treats entire data set as a single cell.
-#' Must be NA or NULL when using stat argument.
 #' @param functions A named list of functions that create new factors which can then be
 #' used by the factors and stat arguments.
 #' @param stat A function that takes the data/the posterior predictives and returns a single value.
@@ -858,7 +857,6 @@ plot_fit <- function(data,pp,subject=NULL,factors=NULL,functions=NULL,
                      q_points=c(.1,.3,.5,.7,.9),
                      qp_cex=1,pqp_cex=.5,lpos="topleft", main = "")
 {
-  if (!is.null(stat) & is.null(factors)) factors <- NA
   if (!is.null(subject)) {
     snams <- levels(data$subjects)
     if (is.numeric(subject)) subject <- snams[subject]
