8d43437cba84c2a2b16d465556b2139585f9f154
Niek Stevenson <niek.stevenson@gmail.com>
2024-07-16 17:32:00 +0200

    fix MomTrunc issue

--- R/model_LBA_extras.R
+++ R/model_LBA_extras.R
@@ -311,6 +311,102 @@ rLBA_TC <- function(pars,ok=rep(TRUE,dim(pars)[1]),return_activation = FALSE)
   out
 }
 
+# Taken from MomTrunc
+recintab0 <- function (kappa, a, b, mu, Sigma)
+{
+  p = length(kappa)
+  seqq = seq_len(p)
+  if (p == 1) {
+    M = matrix(data = 0, nrow = kappa + 1, ncol = 1)
+    s1 = sqrt(Sigma)
+    aa = (a - mu)/s1
+    bb = (b - mu)/s1
+    M[1] = pnorm(bb) - pnorm(aa)
+    if (kappa > 0) {
+      pdfa = s1 * dnorm(aa)
+      pdfb = s1 * dnorm(bb)
+      M[2] = mu * M[1] + pdfa - pdfb
+      if (a == -Inf) {
+        a = 0
+      }
+      if (b == Inf) {
+        b = 0
+      }
+      for (i in seq_len(kappa - 1) + 1) {
+        pdfa = pdfa * a
+        pdfb = pdfb * b
+        M[i + 1] = mu * M[i] + (i - 1) * Sigma * M[i -
+                                                     1] + pdfa - pdfb
+      }
+    }
+  }
+  else {
+    pk = prod(kappa + 1)
+    M = rep(NA, pk)
+    nn = round(pk/(kappa + 1), 0)
+    begind = cumsum(c(0, nn))
+    pk1 = begind[p + 1]
+    cp = matrix(0, p, p)
+    for (i in seqq) {
+      kk = kappa
+      kk[i] = 0
+      cp[i, ] = c(1, cumprod(kk[1:p - 1] + 1))
+    }
+    G = rep(0, pk1)
+    H = rep(0, pk1)
+    s = sqrt(diag(Sigma))
+    pdfa = dnorm(x = a, mean = mu, sd = s)
+    pdfb = dnorm(x = b, mean = mu, sd = s)
+    for (i in seqq) {
+      ind2 = seqq[-i]
+      kappai = kappa[ind2]
+      ai = a[ind2]
+      bi = b[ind2]
+      mui = mu[ind2]
+      Si = Sigma[ind2, i]
+      SSi = Sigma[ind2, ind2] - Si %*% t(Si)/Sigma[i,
+                                                   i]
+      ind = (begind[i] + 1):begind[i + 1]
+      if (a[i] != -Inf) {
+        mai = mui + Si/Sigma[i, i] * (a[i] - mu[i])
+        G[ind] = pdfa[i] * recintab0(kappai, ai, bi,
+                                     mai, SSi)
+      }
+      if (b[i] != Inf) {
+        mbi = mui + Si/Sigma[i, i] * (b[i] - mu[i])
+        H[ind] = pdfb[i] * recintab0(kappai, ai, bi,
+                                     mbi, SSi)
+      }
+    }
+    M[1] = pmvnormt(lower = a, upper = b, mean = mu, sigma = Sigma)
+    a[a == -Inf] = 0
+    b[b == Inf] = 0
+    cp1 = cp[p, ]
+    dims = lapply(X = kappa + 1, FUN = seq_len)
+    grid = do.call(expand.grid, dims)
+    for (i in seq_len(pk - 1) + 1) {
+      kk = as.numeric(grid[i, ])
+      ii = sum((kk - 1) * cp1) + 1
+      i1 = min(seqq[kk > 1])
+      kk1 = kk
+      kk1[i1] = kk1[i1] - 1
+      ind3 = ii - cp1[i1]
+      M[ii] = mu[i1] * M[ind3]
+      for (j in seqq) {
+        kk2 = kk1[j] - 1
+        if (kk2 > 0) {
+          M[ii] = M[ii] + Sigma[i1, j] * kk2 * M[ind3 -
+                                                   cp1[j]]
+        }
+        ind4 = as.numeric(begind[j] + sum(cp[j, ] *
+                                            (kk1 - 1)) - cp[j, j] * kk2 + 1)
+        M[ii] = M[ii] + Sigma[i1, j] * (a[j]^kk2 * G[ind4] -
+                                          b[j]^kk2 * H[ind4])
+      }
+    }
+  }
+  return(M)
+}
 
 n1PDF_MTR_1 <- function(rt, pars,dl,du,b)
   # Works for Balance of Evidence and Threshold count.
@@ -330,7 +426,7 @@ n1PDF_MTR_1 <- function(rt, pars,dl,du,b)
   sv2 <- pars[,"sv"]^2
   sigma <- matrix(c(sv2[1], rep(prod(pars[,"sv"]) * pars[1,"r"], 2), sv2[2]),2, 2, byrow = TRUE)
 
-  Pqpos <- MomTrunc:::recintab0(kappa = rep(0, n),
+  Pqpos <- recintab0(kappa = rep(0, n),
                                 mu = pars[,"v"],
                                 Sigma = sigma,
                                 a = rep(0, n),
@@ -364,7 +460,7 @@ n1PDF_MTR_1 <- function(rt, pars,dl,du,b)
 
     if (x == 1 || x == 3) {
 
-      mom <- MomTrunc:::recintab0(kappa = c(1, 0),
+      mom <- recintab0(kappa = c(1, 0),
                                   mu = pars[,"v"],
                                   Sigma = sigma,
                                   a = c((b - pars[1,"A"]) / t, lb[x]),
@@ -379,7 +475,7 @@ n1PDF_MTR_1 <- function(rt, pars,dl,du,b)
       m_x <- as.vector(m_j + C_j %*% pars[,"v"])
       sigma_x <- C_j %*% sigma %*% t(C_j)
 
-      mom <- MomTrunc:::recintab0(kappa = rep(1, 2),
+      mom <- recintab0(kappa = rep(1, 2),
                                   mu = m_x,
                                   Sigma = sigma_x,
                                   a = c((b - pars[1,"A"]) / t, lb[x]),
