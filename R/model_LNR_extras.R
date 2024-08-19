e729665fa4b86961fd204837e7c2f0bb68aea642
timmerj1 <timmerman.jeroen@gmail.com>
2024-08-19 14:55:34 +0200

    Add noC versions of MRDM and MLNR

--- R/model_LNR_extras.R
+++ R/model_LNR_extras.R
@@ -43,6 +43,42 @@ LNRnoC <- function() {
 #'
 #' @return A model list with all the necessary functions to sample
 #' @export
+MLNRnoC <- function(){
+  list(
+    type="RACE",
+    p_types=c("m" = 1,"s" = log(1),"t0" = log(0),pContaminant=qnorm(0)),
+    p_types=c("m","s","t0","pContaminant"),
+    Ntransform=function(x,use=NULL) {
+      # Transform to natural scale
+      doexp <- !(dimnames(x)[[2]] %in% c("m","pContaminant"))
+      x[,doexp] <- exp(x[,doexp])
+      doprobit <- dimnames(x)[[2]] == "pContaminant"
+      x[,doprobit] <- pnorm(x[,doprobit])
+      x
+    },
+    # p_vector transform scaling parameter by s=1 assumed in lnr.R
+    transform = function(x) x,
+    # Trial dependent parameter transform
+    Ttransform = function(pars,dadm) pars,
+    # Random function for racing accumulators
+    rfun=function(lR=NULL,pars) {
+      if (is.null(lR)) return(rep(TRUE,dim(pars)[1]))
+      rLNR(lR,pars)
+    },
+    # Density function (PDF) for single accumulator
+    dfun=function(rt,pars) dLNR(rt,pars),
+    # Probability function (CDF) for single accumulator
+    pfun=function(rt,pars) pLNR(rt,pars),
+    # Race likelihood combining pfun and dfun
+    log_likelihood=function(p_vector,dadm,min_ll=log(1e-10))
+      log_likelihood_race_missing(p_vector=p_vector, dadm = dadm, min_ll = min_ll)
+  )
+}
+
+#' MLNR: LNR with missing values
+#'
+#' @return A model list with all the necessary functions to sample
+#' @export
 MLNR <- function(){
   list(
     type="RACE",
--- R/model_RDM_extras.R
+++ R/model_RDM_extras.R
@@ -190,6 +190,47 @@ ARDMnoC <- function(){
 #'
 #' @return A list defining the cognitive model
 #' @export
+MRDMnoC <- function(){
+  list(
+    type="RACE",
+    c_name = "MRDM",
+    p_types=c("v" = log(1),"B" = log(1),"A" = log(0),"t0" = log(0),"s" = log(1),
+              pContaminant=qnorm(0)),
+    p_types=c("v","B","A","t0","s","pContaminant"),
+    # Transform to natural scale
+    Ntransform=function(x,use=NULL) {
+      # transform parameters back to real line
+      doprobit <- dimnames(x)[[2]] == "pContaminant"
+      x[,doprobit] <- pnorm(x[,doprobit])
+      x[,!doprobit] <- exp(x[,!doprobit])
+      x
+    },
+    # p_vector transform
+    transform = function(x) x,
+    # Trial dependent parameter transform
+    Ttransform = function(pars,dadm) {
+      attr(pars,"ok") <- (pars[,"t0"] > .05) & ((pars[,"A"] > 1e-6) | pars[,"A"] == 0)
+      pars
+    },
+    # Random function for racing accumulators
+    rfun=function(lR=NULL,pars) {
+      ok <- (pars[,"t0"] > .05) & ((pars[,"A"] > 1e-6) | pars[,"A"] == 0)
+      if (is.null(lR)) ok else rRDM(lR,pars,ok=ok)
+    },
+    # Density function (PDF) for single accumulator
+    dfun=function(rt,pars) dRDM(rt,pars),
+    # Probability function (CDF) for single accumulator
+    pfun=function(rt,pars) pRDM(rt,pars),
+    # Race likelihood combining pfun and dfun
+    log_likelihood=function(p_vector,dadm,min_ll=log(1e-10))
+      log_likelihood_race_missing(p_vector=p_vector, dadm = dadm, min_ll = min_ll)
+  )
+}
+
+#' MRDM: RDM parameterization with missing values
+#'
+#' @return A list defining the cognitive model
+#' @export
 MRDM <- function(){
   list(
     type="RACE",
