a10e813c06a26c8b2000d61b3808072430e4610d
andrewheathcote <andrew.heathcote@newcastle.edu.au>
2024-10-06 14:53:25 +0200

    In get_variant_funs replaced "transform_names" fill in with "transform" and "bound" fill ins (not sure if these are ever used anyway).

--- R/sampling.R
+++ R/sampling.R
@@ -827,10 +827,12 @@ get_variant_funs <- function(type = "standard") {
 calc_ll_manager <- function(proposals, dadm, ll_func, component = NULL){
   if(!is.data.frame(dadm)){
     lls <- log_likelihood_joint(proposals, dadm, component)
-  } else{
-    p_types <- names(attr(dadm,"model")()$p_types)
-    if(is.null(attr(dadm, "transform_names"))){
-      attr(dadm, "transform_names") <- p_types
+  } else {
+    if(is.null(attr(dadm, "transform"))){
+      attr(dadm, "transform") <- attr(dadm,"model")()$transform
+    }
+    if(is.null(attr(dadm, "bound"))){
+      attr(dadm, "bound") <- attr(dadm,"model")()$bound
     }
     c_name <- attr(dadm,"model")()$c_name
     if(is.null(c_name)){ # use the R implementation
